d0eafb7df1312a33f8cdb757521df9003ce73d4d
Vojta <januvojta@gmail.com>
2023-05-16 18:16:47 +0200

    wip

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import ssl
+import hydra
 
 # test python interpreter. If it outputs LibreSSL, then you are using wrong (system) python.
 print(ssl.OPENSSL_VERSION)
+
+
+from omegaconf import DictConfig
+from hydra.utils import to_absolute_path as abspath
+#
+# config: str
+# def init_hydra():
+#     with initialize(version_base=None, config_path="../configs"):
+#         config = compose(config_name="main-config.yaml")
+# def get_data_path():
+#     return abspath("../" + config.data.path)
+
+@hydra.main(version_base=None, config_path="configs", config_name='main-config')
+def process_data(config: DictConfig):
+    """Function to process the data"""
+    raw_path = abspath(config.data.test_path)
+    print(f"Process data using {raw_path}")
+
+if __name__ == '__main__':
+    process_data()
